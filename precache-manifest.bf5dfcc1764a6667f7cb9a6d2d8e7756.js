3cbee67f54e61479b23bedcf96550e089a9626e3
AvHeKZ <66252932+AvHeKZ@users.noreply.github.com>
2020-10-03 15:54:51 -0600

    Updates

---
+++ precache-manifest.bf5dfcc1764a6667f7cb9a6d2d8e7756.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "960936c20358d5e287b6434e2162ba8c",
+    "url": "/Expense-Tracker-React-app/index.html"
+  },
+  {
+    "revision": "1a864aa2aa3205b353f5",
+    "url": "/Expense-Tracker-React-app/static/css/main.e6fd7e02.chunk.css"
+  },
+  {
+    "revision": "f2cf3833cb58d4f57016",
+    "url": "/Expense-Tracker-React-app/static/js/2.c57cdb69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Expense-Tracker-React-app/static/js/2.c57cdb69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a864aa2aa3205b353f5",
+    "url": "/Expense-Tracker-React-app/static/js/main.c99e27cb.chunk.js"
+  },
+  {
+    "revision": "c2f04a7161ee9a30de7a",
+    "url": "/Expense-Tracker-React-app/static/js/runtime-main.eb3aa999.js"
+  }
+]);
