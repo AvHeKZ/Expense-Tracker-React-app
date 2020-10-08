4965a977ec5f0509ae1247d4d0cdc0a41657b2fa
AvHeKZ <66252932+AvHeKZ@users.noreply.github.com>
2020-10-08 14:40:28 -0600

    ini commit

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -18,11 +18,11 @@ const TransactionList = () => {
     }
 
     return (
-        <div className='Container Flipped'>
+        <div>
             <div style={{borderBottom: '1px solid black'}} className=' py-3 mt-2 d-flex justify-content-between align-items-center'>
             <h5 className='m-0'>Zamka's</h5> <img src={giphy} alt="loading..." style={{width: '35px', height: '35px'}} /> <button  onClick={clearAll}className='float-right btn btn-dark text-center btn-sm'>Clear All</button>
             </div>
-            <ul className='list-group  '>
+            <ul className='list-group Container Flipped '>
                 {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction}/>))}
             </ul>
         </div>
